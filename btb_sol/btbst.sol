c6bbe4d903e9d518b4b36d6402b2818ee974cf83
nicoleolivetto <111580428+nicoleolivetto@users.noreply.github.com>
2024-02-20 18:34:36 +0100

    Add files via upload
    This file contains the solution of the Back-to.back equity measure

---
+++ btb_sol/btbst.sol
@@ -0,0 +1,7 @@
+Number of students with back-to-back exams after optimization: 0
+Total Penalty: 3.375
+Exam Schedule:
+Exam 0001 is scheduled at time slot 6
+Exam 0002 is scheduled at time slot 3
+Exam 0003 is scheduled at time slot 1
+Exam 0004 is scheduled at time slot 1
